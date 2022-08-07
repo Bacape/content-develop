66a065a5774a2df8a4a400ede86f2c8fc9386ce2
Ondrej Ochodnicky <ondrej.ochodnicky@cloudtalk.io>
2022-08-07 11:05:14 +0200

    scroll spy

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('.scroll-spy');
+const hero = document.querySelector('.hero');
+const header = document.querySelector('.header');
+const headerHeight = header.clientHeight;
+
 function scrollTo(element) {
-    const headerHeight = document.querySelector('.header').clientHeight;
     window.scroll({
         behavior: 'smooth',
         left: 0,
@@ -13,3 +18,33 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
         scrollTo(document.querySelector(this.getAttribute('href')));
     });
 });
+
+// fixing hero fullscreen with sticky header
+hero.style.marginTop = `-${headerHeight}px`;
+
+window.onscroll = () => {
+    const currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+    const viewHeight = window.innerHeight;
+
+    sections.forEach((v, i) => {
+        const elementOffset = v.offsetTop;
+        const elementHeight = v.clientHeight;
+
+        // is scrolled more than element from top or 10% of element height from bottom
+        if (
+            currentScroll - headerHeight >= elementOffset ||
+            currentScroll + viewHeight - elementHeight * 0.1 >= elementOffset
+        ) {
+            // remove all current active classes
+            navLinks.forEach((v) => v.classList.remove('active'));
+            document
+                .querySelector(`a[href*=${sections[i].id}]`)
+                .classList.add('active');
+        } else {
+            document
+                .querySelector(`a[href*=${sections[i].id}]`)
+                .classList.remove('active');
+        }
+    });
+};
